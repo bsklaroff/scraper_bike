0e7e6b5c8b28657e242f679ebf045cd68f99870f
Lu Cheng <lu.cheng@berkeley.edu>
2012-04-29 07:02:47 -0700

    working one page parser

--- one_page_parser.py
+++ one_page_parser.py
@@ -7,13 +7,14 @@ import re
 def matches_input(tag):
     return tag.find(string_to_match) != -1
 
-string_to_match = "Botanical Garden:  Spring plant sale"
-
+#string_to_match = "Recital:  Sandrine Piau, soprano, Susan Manoff, piano"
+string_to_match = sys.argv[2]
 def main():
     #url = sys.argv[1]
     url = "http://en.wikipedia.org/wiki/Web_scraping"
     url = "http://sfbay.craigslist.org/nby/muc/2985476465.html"
-    url = "http://events.berkeley.edu/"
+    #url = "http://events.berkeley.edu/"
+    url = sys.argv[1]
     opener = urllib2.build_opener()
     opener.addheaders = [('User-agent', 'Mozilla/5.0')]
     web_page = opener.open(url)
@@ -48,24 +49,68 @@ def main():
     soup = BeautifulSoup(web_page.read())
     path.reverse()
     current_element = soup
-    for node in path:
+
+    greatest_false = 1
+    best_match = []
+    for i in range(len(path)):
+        for j in range(len(path)):
+            test_values = recursive_match(path, i, j, current_element)
+            if not isinstance(test_values, list):
+                continue
+            if not test_values:
+                continue
+            if len(test_values) > 300 or len(test_values) < 2:
+                continue
+            false_count = 0
+            for x in test_values:
+                if x == False or x is None:
+                    false_count += 1
+            false_count /= (len(test_values) * 1.0)
+            if false_count < greatest_false:
+                best_match = test_values
+                greatest_false = false_count
+            #print test_values
+    print best_match
+    """for node in path:
         tag_type_elements = current_element.find_all(node[0])
         element = tag_type_elements[node[2]]
+        
+
         if element.attrs == node[1]:
             print "Attribute matched"
         else:
             print "Attribute did not match"
         print element.attrs, node[1]
         #print element.name, node[0]
-        current_element = element
-    
-    
-    print current_element
+        current_element = element    
+    print "\n\n\n" + current_element.string """
         
 #for child in list(soup.contents):
         #print child
 
 
+#(element_name, element attrs, parents_nth_tag)
+def recursive_match(path, i, j, current_element):
+    if i == len(path) - 1:
+        return current_element.string
+    node = path[i]
+    tag_type_elements = current_element.find_all(node[0])
+    if i == j:
+        return [recursive_match(path, i + 1, j, next_element) for next_element in tag_type_elements]
+    else:
+        if len(tag_type_elements) <= node[2]:
+            return False
+        next_element = tag_type_elements[node[2]]
+        if next_element.attrs == node[1]:
+            x = 1
+#print "Attribute matched"
+        else:
+            x = 2
+            #print "Attribute did not match"
+        current_element = next_element
+        return recursive_match(path, i + 1, j, current_element)
+    
+    
 
 
 if __name__ == "__main__":
